1e82b58727c0a388cc2102633be352aa8f95d2b0
David Haberthür <email@davidhaberthuer.ch>
2022-04-29 21:26:06 +0200

    A colon at the end would be nice, thanks.

--- generate.php
+++ generate.php
@@ -40,7 +40,7 @@ foreach($feed->get_items() as $item) {
   $article['host'] = str_replace('www.', '', $article['parseurl']['host']);
   //$article['comment'] = html_entity_decode($item->get_content());
   // Generate Output
-  echo "- [".$article['title']."](".$article['url'].") *(".$article['host'].")*";
+  echo "- [".$article['title']."](".$article['url'].") *(".$article['host'].")* :";
   //echo  $article['comment'];
   echo "\n";
 }
